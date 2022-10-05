f1026500c99be6f8b9b268955eb6f822a1394496
Thierry <270067227@yoobeestudent.ac.nz>
2022-10-06 11:15:13 +1300

    fixed x1 & x2 outputting inverted
    fixed "roots are imaginary" defaulting to "roots are real and unequal"

--- CS102 - Task 1/task_1.cpp
+++ CS102 - Task 1/task_1.cpp
@@ -6,21 +6,27 @@
 */
 
 
+/*
+ Equasions:
+	x1 = (-numB + sqrt(numB * numB - 4 * numA * numC)) / (2 * numA);
+	x2 = (-numB - sqrt(numB * numB - 4 * numA * numC)) / (2 * numA);
+ */
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-float taskCalcX1(float x1, float numA, float numB, float numC)
+float realCalcX1(float x1, float numA, float numB, float numC)
 {
-    x1 = (numB + sqrt(numB * numB - 4 * numA * numC)) / (2 * numA);
+    x1 = numB * numB - 4 * numA * numC;
 
     return x1;
 }
 
-float taskCalcX2( float x2, float numA, float numB, float numC)
+float realCalcX2( float x2, float numA, float numB, float numC)
 {
-    x2 = (numB - sqrt(numB * numB - 4 * numA * numC)) / (2 * numA);
+    x2 = numB * numB - 4 * numA * numC;
 
     return x2;
 }
@@ -28,22 +34,21 @@ float taskCalcX2( float x2, float numA, float numB, float numC)
 int main()
 {
     //var
-    float numA;
-    float numB;
-    float numC;
+    float numA, numB, numC;
     float x1{}, x2{};
-    float findRealX1;
-    float findRealX2;
+    float findRealX1, findRealX2;
+	float calcX1, calcX2;
+
     //console input
     cout << "Enter a, b, c: ";
     cin >> numA >> numB >> numC;
 
-    findRealX1 = taskCalcX1(x1, numA, numB, numC);
-    findRealX2 = taskCalcX2(x2, numA, numB, numC);
+    findRealX1 = realCalcX1(x1, numA, numB, numC);
+    findRealX2 = realCalcX2(x2, numA, numB, numC);
 
-    if (findRealX1 > 0 && findRealX2 > 0) {
+    if (findRealX1 < 0 && findRealX2 < 0) {
         //do this
-        cout << "roots are real and unequal";
+        cout << "roots are imaginary";
     }
     else if (findRealX1 == 0 && findRealX2 == 0) {
         //do that
@@ -51,12 +56,15 @@ int main()
     }
     else {
         //break
-        cout << "roots are imaginary";
+        cout << "roots are real and unequal";
     }
 
+	calcX1 = (-numB + sqrt(findRealX1)) / (2 * numA);
+	calcX2 = (-numB - sqrt(findRealX2)) / (2 * numA);
 
-    cout << endl << "x1: " << findRealX1 << "\t";
-    cout << "x2: " << findRealX2 << endl;
+
+	cout << endl << "x1: " << calcX1 << "\t";
+    cout << "x2: " << calcX2 << endl;
 
 
 }
