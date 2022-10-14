1629766f4236fb26dadaac3921b24794792c37c2
Thierry <270067227@yoobeestudent.ac.nz>
2022-10-14 13:08:17 +1300

    fixed comments and random # generator

--- CS102 - Task 5/task_5.cpp
+++ CS102 - Task 5/task_5.cpp
@@ -5,26 +5,31 @@
 //
 
 #include <iostream>
-#include <cstdlib>
 #include <iomanip>  //iosteam manip
 
 using namespace std;
 
 int main() {
+
+    //  Variables
     int x, guess;
 
-    srand((unsigned)time(NULL));
-    x = (rand() % 11);
+    srand((unsigned)time(NULL));  //setup random seed
+    x = rand() % 10 + 1;          //generate random number
 
-    //Input area??
+    //  Title - Centered
     cout << setw (25) << "Guess Game" << endl;
     cout << setw (25) << "**********" << endl;
+
+    //  Output: Rules
     cout << "\nYou have three attempts.\nGuess the correct number between 1 to 10.\n\n";
 
-    for (int i=0; i < 3; i++) {
-        cout << "\nChoice " << i+1 << ": ";
+    //  Guess Game
+    for (int i=0; i < 3; i++) {     //Loop 3 times
+        cout << "\nChoice " << i+1 << ": ";   //Display Attempt #
         cin >> guess;
 
+        //  Check guess against correct number
         if (x == guess){
             cout << "\nCorrect!\n";
             break;
